0ac5746e7ae628966e4e8fc1b13a3f14a353bf41
Abdul <rahimi.diallo96@gmail.com>
2021-11-06 17:03:15 -0400

    init project

---
+++ NetworkRequestor/NetworkRequestor.h
@@ -0,0 +1,18 @@
+//
+//  NetworkRequestor.h
+//  NetworkRequestor
+//
+//  Created by Abdul Diallo on 11/6/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for NetworkRequestor.
+FOUNDATION_EXPORT double NetworkRequestorVersionNumber;
+
+//! Project version string for NetworkRequestor.
+FOUNDATION_EXPORT const unsigned char NetworkRequestorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NetworkRequestor/PublicHeader.h>
+
+
